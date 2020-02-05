17a9597518d334b859b0dc6ac6295c136cfc89e9
Liron Ilouz <liron@tapwithus.com>
2020-02-05 13:10:31 +0200

    * ble device class

---
+++ TapSDK/device.py
@@ -0,0 +1,27 @@
+import abc
+
+
+class BlePeripheral(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def connect(self, name: str):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def enable_notification(self, characteristic, callback):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def disable_notification(self, characteristic):
+        raise NotImplementedError()
+    
+    @abc.abstractmethod
+    def read_from_char(self, characteristic):
+        raise NotImplementedError()
+    
+    @abc.abstractmethod
+    def write_to_char(self, characteristic):
+        raise NotImplementedError()
+
