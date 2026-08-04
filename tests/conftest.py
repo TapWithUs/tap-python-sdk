1cae7e1277568ddd815abadc60e702291a10d393
Itai Ofir <itai@tapwithus.com>
2026-08-04 14:57:29 +0300

    linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,16 +13,23 @@ def pytest_configure(config):
         def __init__(self, *args, **kwargs):
             pass
 
+    def noop(*a, **k):
+        pass
+
     stub("bleak", BleakClient=Dummy, BleakScanner=Dummy,
-         _logger=types.SimpleNamespace(debug=lambda *a, **k: None, info=lambda *a, **k: None, error=lambda *a, **k: None))
+         _logger=types.SimpleNamespace(debug=noop, info=noop, error=noop))
     stub("bleak.backends")
     stub("bleak.backends.device", BLEDevice=Dummy)
     stub("bleak.backends.bluezdbus", defs=types.SimpleNamespace(
-        BLUEZ_SERVICE="", DEVICE_INTERFACE="", OBJECT_MANAGER_INTERFACE="", PROPERTIES_INTERFACE=""))
-    stub("bleak.backends.bluezdbus.utils", assert_reply=lambda *a: None, unpack_variants=lambda v: v)
-    stub("dbus_next", BusType=types.SimpleNamespace(SYSTEM="system"), Message=Dummy, Variant=Dummy)
+        BLUEZ_SERVICE="", DEVICE_INTERFACE="",
+        OBJECT_MANAGER_INTERFACE="", PROPERTIES_INTERFACE=""))
+    stub("bleak.backends.bluezdbus.utils",
+         assert_reply=lambda *a: None, unpack_variants=lambda v: v)
+    stub("dbus_next", BusType=types.SimpleNamespace(SYSTEM="system"),
+         Message=Dummy, Variant=Dummy)
     stub("dbus_next.aio", MessageBus=Dummy)
     stub("bleak.backends.corebluetooth.CentralManagerDelegate",
          CBUUID=type("CBUUID", (), {"UUIDWithString_": staticmethod(lambda x: x)}),
          CentralManagerDelegate=type("CentralManagerDelegate", (), {
-             "alloc": classmethod(lambda cls: type("Obj", (), {"init": lambda self: None})())}))
+             "alloc": classmethod(
+                 lambda cls: type("Obj", (), {"init": lambda self: None})())}))
