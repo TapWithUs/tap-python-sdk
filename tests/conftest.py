d3c6f8c9bf815b881ec1491d1439de359e4b8304
Itai Ofir <itai@tapwithus.com>
2026-08-04 14:51:22 +0300

    conftest stubbing for new bleak linux path

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,26 +3,26 @@ import types
 
 
 def pytest_configure(config):
-    bleak_stub = types.ModuleType("bleak")
+    def stub(name, **attrs):
+        mod = sys.modules.setdefault(name, types.ModuleType(name))
+        for key, value in attrs.items():
+            setattr(mod, key, value)
+        return mod
 
     class Dummy:
         def __init__(self, *args, **kwargs):
             pass
 
-    bleak_stub.BleakClient = Dummy
-    bleak_stub.BleakScanner = Dummy
-    bleak_stub._logger = types.SimpleNamespace(
-        debug=lambda *a, **k: None,
-        info=lambda *a, **k: None,
-        error=lambda *a, **k: None,
-    )
-    sys.modules.setdefault("bleak", bleak_stub)
-
-    core_mod = types.ModuleType("bleak.backends.corebluetooth.CentralManagerDelegate")
-    core_mod.CBUUID = type("CBUUID", (), {"UUIDWithString_": staticmethod(lambda x: x)})
-    core_mod.CentralManagerDelegate = type(
-        "CentralManagerDelegate",
-        (),
-        {"alloc": classmethod(lambda cls: type("Obj", (), {"init": lambda self: None})())},
-    )
-    sys.modules.setdefault("bleak.backends.corebluetooth.CentralManagerDelegate", core_mod)
+    stub("bleak", BleakClient=Dummy, BleakScanner=Dummy,
+         _logger=types.SimpleNamespace(debug=lambda *a, **k: None, info=lambda *a, **k: None, error=lambda *a, **k: None))
+    stub("bleak.backends")
+    stub("bleak.backends.device", BLEDevice=Dummy)
+    stub("bleak.backends.bluezdbus", defs=types.SimpleNamespace(
+        BLUEZ_SERVICE="", DEVICE_INTERFACE="", OBJECT_MANAGER_INTERFACE="", PROPERTIES_INTERFACE=""))
+    stub("bleak.backends.bluezdbus.utils", assert_reply=lambda *a: None, unpack_variants=lambda v: v)
+    stub("dbus_next", BusType=types.SimpleNamespace(SYSTEM="system"), Message=Dummy, Variant=Dummy)
+    stub("dbus_next.aio", MessageBus=Dummy)
+    stub("bleak.backends.corebluetooth.CentralManagerDelegate",
+         CBUUID=type("CBUUID", (), {"UUIDWithString_": staticmethod(lambda x: x)}),
+         CentralManagerDelegate=type("CentralManagerDelegate", (), {
+             "alloc": classmethod(lambda cls: type("Obj", (), {"init": lambda self: None})())}))
